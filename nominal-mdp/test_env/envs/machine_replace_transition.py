c1bcfe6c18d41ee74d032f72dd8f5aebd2513f2f
Ayan Biswas <ayan_biswas@eecs.berkeley.edu>
2018-12-07 15:15:51 -0800

    update machine replacement env

--- nominal-mdp/test_env/envs/machine_replace_transition.py
+++ nominal-mdp/test_env/envs/machine_replace_transition.py
@@ -7,12 +7,16 @@ from gym.envs.toy_text import discrete
 
 class MachineReplacementDirichletEnv(discrete.DiscreteEnv):
     def __init__(self, desc=None):
+        pass
+
+    def set(self, config):
         nA = 2
         nS = 10
         done = False
         Q = {s: {a: [] for a in range(nA)} for s in range(nS)}
         P = {s: {a: [] for a in range(nA)} for s in range(nS)}
 
+        eps = config.eps
         # 8 represents R1, 9 represents R2
         for s in range(nS):
             for a in range(nA):
@@ -22,16 +26,16 @@ class MachineReplacementDirichletEnv(discrete.DiscreteEnv):
                 if a == 1:
                     # update state
                     if s == 7:
-                        eps = np.random.rand() / 2
+                        # eps = np.random.rand() / 2
                         li.append((0.6, 8, 2, done))
                         li.append((0.1, 9, 10, done))
                         li.append((0.3, 7, 20, done))
-                        lli.append((0.6-eps, 8, 2, done))
-                        lli.append((0.1+eps, 9, 10, done))
+                        lli.append((0.6-eps/2, 8, 2, done))
+                        lli.append((0.1+eps/2, 9, 10, done))
                         lli.append((0.3, 7, 20, done))
                     elif s == 8:
-                        # eps = np.random.rand() / 2
-                        eps = 1.0
+                        # eps = np.random.rand()
+                        # eps = 1.0
                         lli.append((1.0-eps, 8, 2, done))
                         lli.append((eps, 0, 2, done))
                         li.append((1.0, 8, 2, done))
@@ -41,7 +45,7 @@ class MachineReplacementDirichletEnv(discrete.DiscreteEnv):
                         li.append((0.6, 8, 2, done))
                         li.append((0.4, 9, 10, done))
                     else:
-                        eps = np.random.rand() / 2
+                        # eps = np.random.rand() / 2
                         li.append((0.6, 8, 2, done))
                         li.append((0.1, 9, 10, done))
                         if s == 6:
@@ -62,11 +66,11 @@ class MachineReplacementDirichletEnv(discrete.DiscreteEnv):
                     elif s == 8:
                         li.append((0.2, 8, 0, done))
                         li.append((0.8, 0, 0, done))
-                        eps = np.random.rand() / 2
-                        lli.append((0.2 + eps, 8, 0, done))
-                        lli.append((0.8 - eps, 0, 0, done))
+                        # eps = np.random.rand() / 2
+                        lli.append((0.2 + eps / 2, 8, 0, done))
+                        lli.append((0.8 - eps / 2, 0, 0, done))
                     elif s == 9:
-                        eps = np.random.rand() / 2
+                        # eps = np.random.rand()
                         li.append((1.0, 9, 0, done))
                         lli.append((1.0 - eps, 9, 0, done))
                         lli.append((eps, 8, 0, done))
